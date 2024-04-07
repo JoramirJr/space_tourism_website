d0ca17c0f23673fbc06217a62f6830988dd55704
Joramir da Silva Junior <joramirjunior@gmail.com>
2024-04-07 17:15:47 -0300

    .nojekyll to build dir

---
+++ build/_app/immutable/chunks/paths.Cincp_qb.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_rfzhte)==null?void 0:s.base)??"/space_tourism_website";var e;const a=((e=globalThis.__sveltekit_rfzhte)==null?void 0:e.assets)??t;export{a,t as b};
